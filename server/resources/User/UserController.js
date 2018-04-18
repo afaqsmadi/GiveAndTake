0b14490ab1aecf566c6d25139f179c24bd8d54cb
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-04-18 10:39:24 +0300

    base files to start our project

---
+++ server/resources/User/UserController.js
@@ -0,0 +1,15 @@
+var User = require('./User');
+
+// Complete each of the following controller methods
+
+exports.createOne = function (req, res) {
+	
+};
+
+exports.login = function (req, res) {
+
+};
+
+exports.logout = function (req, res) {
+
+};
