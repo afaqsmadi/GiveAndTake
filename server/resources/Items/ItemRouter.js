42f11471e227cb4c09358fc030e2f8314ccd6477
montheramer <monther.amer48@gmail.com>
2018-04-19 11:26:59 +0300

    working on items

--- server/resources/Items/ItemRouter.js
+++ server/resources/Items/ItemRouter.js
@@ -4,15 +4,23 @@ var ItemController = require('./ItemController');
 // Create route handlers for each of the six methods in pokemonController
 ItemRouter.route('/')
 	.get(function (req, res) {
-		res.json("get request");
+		ItemController.searchItem(req,res);
+		//res.json("get request");
+
 		//ItemController.searchItem(req , res); // login
 
 	})
 	.post(function (req, res) {
-		ItemController.createItem(req, res); // createUser
+		ItemController.createItem(req, res); // createItem
 	})
-	// .put(function (req, res) {
-	// 	ItemController.updateItem(req, res); // updateUser
-	// })
+	.put(function (req, res) {
+		// console.log("asdasdas")
+		ItemController.updateItem(req, res); // updateItem
+	})
+	.delete(function (req, res) {
+		// console.log("asdasdas")
+		ItemController.deleteItem(req, res); // deleteItem
+	})
+
 
 module.exports = ItemRouter;
