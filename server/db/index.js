2f3ac9743ca51def9aa4fee95a0cdb27156ff86c
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-04-26 18:55:46 +0300

    use heroku var

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/Medusa';
-var mlab = 'mongodb://give:take@ds159509.mlab.com:59509/giveandtake'
-mongoose.connect(mlab, { useMongoClient: true });
+mongoose.connect(process.env.MONGOURI , { useMongoClient: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console,'connection to database not working'));
 db.once('open', function() {
